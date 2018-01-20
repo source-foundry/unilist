1b552d61377bb2503954187a0fd48272da35cf19
Chris Simpkins <git.simpkins@gmail.com>
2018-01-20 14:54:59 -0500

    [unilist.go] refactored utilities and parser functions out of source file

--- unilist.go
+++ unilist.go
@@ -5,12 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
 const (
-	version = "0.1.0"
+	version = "0.2.0"
 
 	usage = `Usage: unilist (options) [arg 1]...[arg n]
 `
@@ -118,55 +117,10 @@ func main() {
 
 	// format the standard out string with proper default or command line defined delimiter
 	if *useCommasShort || *useCommasLong {
-		fmt.Print(strings.Join(keepUniList, ","))
+		fmt.Print(getCommaDelimitedString(keepUniList))
 	} else if *useNewLinesShort || *useNewLinesLong {
-		fmt.Print(strings.Join(keepUniList, "\n"))
+		fmt.Print(getNewlineDelimitedString(keepUniList))
 	} else {
-		fmt.Print(strings.Join(keepUniList, " "))
+		fmt.Print(getSpaceDelimitedString(keepUniList))
 	}
-}
-
-func getUnicodeValueSlice(arg string) ([]string, error) {
-	var returnSlice []string
-	// strip the case insensitive 'x' value from exclude request arguments (detected in calling code)
-	if strings.HasPrefix(arg, "x") || strings.HasPrefix(arg, "X") {
-		arg = arg[1:]
-	}
-	if strings.Contains(arg, "-") {
-		tempUniList := strings.Split(arg, "-")
-		startInt, startErr := strconv.ParseInt(tempUniList[0], 16, 32)
-		endInt, endErr := strconv.ParseInt(tempUniList[1], 16, 32)
-		if startErr != nil {
-			return returnSlice, startErr
-		}
-		if endErr != nil {
-			return returnSlice, endErr
-		}
-		for i := startInt; i <= endInt; i++ {
-			returnSlice = append(returnSlice, fmt.Sprintf("%U", i))
-		}
-	} else {
-		if len(arg) > 3 {
-			anInt, parseErr := strconv.ParseInt(arg, 16, 32)
-			if parseErr != nil {
-				return returnSlice, parseErr
-			}
-			returnSlice = append(returnSlice, fmt.Sprintf("%U", anInt))
-
-		} else {
-			// handle argument that is not formatted as unicode hexadecimal value
-			lenErr := fmt.Errorf("the argment '%s' is not properly formatted as a Unicode hexadecimal value", arg)
-			return returnSlice, lenErr
-		}
-	}
-	return returnSlice, nil
-}
-
-func containsString(haystack []string, needle string) bool {
-	for _, a := range haystack {
-		if a == needle {
-			return true
-		}
-	}
-	return false
 }
